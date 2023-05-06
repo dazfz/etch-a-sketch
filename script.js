496eb694e64a49ae735c364aff3609fb84863f10
dazfz <dazhi891@gmail.com>
2023-05-06 10:08:39 -0400

    modified:   script.js

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ btn.addEventListener("click", () => {
   } else alert("Invalid input. Please enter a positive integer less than 101.");
 });
 
-const main = document.querySelector("main");
+const main = document.querySelector("main"); // seleccionar main
 function createGrid(size) {
   main.innerHTML = ""; // resetea el contenido de main
   for (let i = 0; i < size * size; i++) {
+    // crear div hijo de main
     const div = document.createElement("div");
     main.appendChild(div);
 
@@ -25,6 +26,7 @@ function createGrid(size) {
     ];
     let darkness = 0;
 
+    // si se mantiene el mouse apretado, y se pasa por encima de un div, se colorea
     div.addEventListener("mousedown", () => (isMouseDown = true));
     div.addEventListener("mouseup", () => (isMouseDown = false));
     div.addEventListener("mouseover", () => {
